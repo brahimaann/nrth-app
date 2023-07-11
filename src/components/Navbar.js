bd821922b1e3b7169e8f524e6a1478dedb52039d
brahimaann <82335349+brahimaann@users.noreply.github.com>
2023-07-11 18:13:02 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,9 +20,11 @@ function Navbar() {
                 <li>
                     <a href="/about">About</a>
                 </li>
-              //  <li>
-                //    <a href="/projects">Projects</a>
-               // </li>
+{/*  
+                <li>
+                   <a href="/projects">Projects</a>
+                </li>
+*/}
             </ul>
         </nav>);
 }
